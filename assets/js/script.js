01c18295955b07202466c9e5019df087797f4e98
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-05 16:28:20 +0300

    the properties for the hover effect for the nav bar has been modified

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+let btnEl = document.getElementsByClassName("submit_btn");
+
+
+
+function callFunction(){
+    alert("our Message has been received. Thank you for contacting Earnest! I will get back to you asap!")
+}
